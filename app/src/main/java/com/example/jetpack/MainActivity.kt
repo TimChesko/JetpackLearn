22f6c257be19ce6c9447a6357eb26d04966b6cef
TimChesko <timofey.lazarev@gmail.com>
2022-02-01 12:29:19 +0300

    Не понятно работает тема, вроде цвет шрифта меняется, но не меняется фон

--- app/src/main/java/com/example/jetpack/MainActivity.kt
+++ app/src/main/java/com/example/jetpack/MainActivity.kt
@@ -4,22 +4,26 @@ import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.animateContentSize
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.*
 import com.example.jetpack.ui.theme.JetpackTheme
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.Surface
+import androidx.compose.runtime.*
+import androidx.compose.ui.graphics.Color
 
 
 class MainActivity : ComponentActivity() {
@@ -27,7 +31,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             JetpackTheme() {
-                MessageCard(Message("Android", "Jetpack Compose"))
+                Conversation(SampleData.conversationSample)
             }
         }
     }
@@ -47,32 +51,50 @@ fun MessageCard(msg: Message) {
                 .border(1.5.dp, MaterialTheme.colors.secondary, CircleShape) //добавил шрину обврдки и цвет
         )
         Spacer(modifier = Modifier.width(8.dp)) //промежуток по горизонатли между картинкой и текстом
-        Column {
+        var isExpanded by remember { mutableStateOf(false) }
+        val surfaceColor: Color by animateColorAsState(
+            if (isExpanded) MaterialTheme.colors.primary else MaterialTheme.colors.surface,
+        )
+        Column(modifier = Modifier.clickable { isExpanded = !isExpanded }) {
             Text(
                 text = msg.author, //вывели кто написал
                 color = MaterialTheme.colors.secondaryVariant, //изменили цвет текста
                 style = MaterialTheme.typography.subtitle2 //изменил шрифт
             )
             Spacer(modifier = Modifier.height(4.dp)) //промежуток по вертикали между строками
-            Text(text = msg.body, //вывели сообщение
-            style = MaterialTheme.typography.body2 //изменил шрифт
-            )
+            Surface(shape = MaterialTheme.shapes.medium, elevation = 1.dp,color = surfaceColor,modifier = Modifier.animateContentSize().padding(1.dp)){
+                Text(
+                    text = msg.body,
+                    modifier = Modifier.padding(all = 4.dp),
+                    maxLines = if (isExpanded) Int.MAX_VALUE else 1,
+                    style = MaterialTheme.typography.body2
+                )
+            }
         }
     }
 }
-@Preview(
-    name = "LightMode"
-)
-@Preview(
-    uiMode = Configuration.UI_MODE_NIGHT_YES,
-    showBackground = true,
-    name = "DarkMode"
-)
+//@Preview(
+//    name = "LightMode"
+//)
+//@Preview(
+//    uiMode = Configuration.UI_MODE_NIGHT_YES,
+//    showBackground = true,
+//    name = "DarkMode"
+//)
+//@Composable
+//fun PreviewMessageCard() {
+//    JetpackTheme() {
+//        MessageCard(
+//            Conversation(SampleData.conversationSample)
+//        )
+//    }
+//}
+
 @Composable
-fun PreviewMessageCard() {
-    JetpackTheme() {
-        MessageCard(
-            msg = Message("Colleague", "Hey, take a look at Jetpack Compose, it's great!")
-        )
+fun Conversation(messages: List<Message>) {
+    LazyColumn {
+        items(messages) { message ->
+            MessageCard(message)
+        }
     }
 }
